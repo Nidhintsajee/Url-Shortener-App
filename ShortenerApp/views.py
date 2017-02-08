69bd341c2833ef052d9782d0bea18c6647fad6a7
Nidhintsajee <nidhintsajee@gmail.com>
2017-02-08 16:05:42 +0530

    My Url Shortener App

--- ShortenerApp/views.py
+++ ShortenerApp/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+import random, string, json
+from ShortenerApp.models import Urls
+from django.http import HttpResponseRedirect, HttpResponse
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def index(request):
+    c = {}
+    c.update(request)
+    return render_to_response('ShortenerApp/index.html', c)
+
+def redirect_original(request, short_id):
+    url = get_object_or_404(Urls, pk=short_id)
+    url.count += 1
+    url.save()
+    return HttpResponseRedirect(url.http)
+
+@csrf_exempt
+def shorten_url(request):
+    url = request.POST.get("url", '')
+    if not (url == ''):
+        short_id = get_short_code()
+        b = Urls(httpurl=url, short_id=short_id)
+        b.save()
+
+        response_data = {}
+        response_data['url'] = settings.SITE_URL + "/" + short_id
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
+
+
+def get_short_code():
+    length = 6
+    char = string.ascii_uppercase + string.digits + string.ascii_lowercase
+    # if the randomly generated short_id is used then generate next
+    while True:
+        short_id = ''.join(random.choice(char) for x in range(length))
+        try:
+            temp = Urls.objects.get(pk=short_id)
+        except:
+            return short_id
